814d215aa0e34e0b7474645e5354545ef6cbcadf
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2019-11-03 21:18:07 -0800

    poc for api connecting to gatsby site complete

---
+++ api/src/constants/responses.js
@@ -0,0 +1,7 @@
+export const RESPONSES = {
+  asText: {
+    200: 'OK',
+    400: 'BAD REQUEST',
+    401: 'NOT AUTHORIZED'
+  }
+}
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -4,13 +4,19 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-
+import axios from 'axios';
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <button onClick={() => {
+      axios.get('http://localhost:3000/ehlo')
+      .then(data => {
+        console.log(data)
+      })
+    }}>CLICK ME!!</button>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
